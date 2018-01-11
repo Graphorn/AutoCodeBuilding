746a9a24343a5dc7760539877d0fdc43fd32b1a4
husterxsp <husterxsp@qq.com>
2018-01-11 22:19:27 +0800

    update

--- auto-code-building/app/controllers/application_controller.rb
+++ auto-code-building/app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
+    layout false
     protect_from_forgery with: :exception
     def index
-        render file: 'public/index.html'
+
+        # render 'layouts/mailer.html.erb'
+        render 'layouts/index.html.erb'
+        
+        # render file: 'public/index.html'
+        # render :file => 'index.html'
     end
 end
