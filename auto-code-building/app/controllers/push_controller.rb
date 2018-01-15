4f74c5f34b15b2e6482276317469166be0d668b0
Lapidary <xy3839401@163.com>
2018-01-15 01:50:32 +0000

    shell

--- auto-code-building/app/controllers/push_controller.rb
+++ auto-code-building/app/controllers/push_controller.rb
@@ -15,46 +15,21 @@ class PushController < ApplicationController
         logs = ""
         Open3.popen3(command1) do |stdin, stdout, stderr|
         #     # puts "a."
-            stderr.each_line {|line| logs << line.chomp }
+            # stderr.each_line {|line| logs << line.chomp }
+            stderr.each_line {|line| logs << line }
+            # stderr.close
         end
-        #     # puts "b."
-        #     # stdout.each_line { |line| puts line }
-        # puts "stderr"
-        # puts stderr.gets
-        #发送post请求
-        # post_params = {}  
-        # @userName = params[:user_name]
-        # @projectName = params[:project_name]
-        # if User.find_by(user_name:@userName) and Project.find_by(project_name:@projectName)
-        #     @buildInfo = Buildinfo.new
-        #     @buildInfo.user_name = @userName
-        #     @buildInfo.loginfo = params[:log_info]
-        #     @buildInfo.project = @projectName
-        #     @buildInfo.project_url = params[:project_url]
-        #     @buildInfo.author = params[:author]
-        #     @buildInfo.branch = params[:branch]
-        #     @buildInfo.commit_url = params[:commit_url]
-        #     @buildInfo.commmit_msg = params[:commit_msg]
-        #     @buildInfo.build_time = params[:build_time]
-        #     @buildInfo.build_status = params[:build_status]
-        # post_params["username"] = params[:repository][:owner][:name]
-        # post_params["project"] = params[:repository][:name]
-        # post_params["loginfo"] = logs
-        # post_params["project_url"] = url
-        # post_params["author"] = params[:repository][:owner][:name]
-        # post_params["branch"] = params[:repository][:default_branch]
-        # post_params["commit_url"] = params[:commits][0][:url]
-        # post_params["commmit_msg"] = params[:commits][0][:message]
-        # puts post_params
-        # uri = URI.parse("https://limiao-limiao.c9users.io/buildinfos/addbuildinfo")  
-        # res = Net::HTTP.post_form(uri, post_params)  
-        # #返回的cookie  
-        # puts res.header['set-cookie']  
-        # #返回的html body  
-        # puts res.body  
+        puts "logs:"
+        puts logs
+        # 编译是否成功
+        status = "false"
+        if logs==''
+            status = "true"
+        end
+            
         addBuild(params[:repository][:owner][:name], params[:repository][:name], logs, 
                     params[:repository][:owner][:name],params[:repository][:default_branch],
-                    params[:commits][0][:url], params[:commits][0][:message], "true")
+                    params[:commits][0][:url], params[:commits][0][:message], status)
         
         # Open3.popen3(command1) do |stdin, stdout, stderr|
         #     # puts "a."
--- auto-code-building/compile.sh
+++ auto-code-building/compile.sh
@@ -1,3 +1,4 @@
 git clone $1 $2 >/dev/null 2>/dev/null
 cd $2
+bundle install
 rake
